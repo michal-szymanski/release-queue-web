79b582c44c4455122e317fef52df6672ffedcf4f
Michał Szymański <michal.szymanski92@gmail.com>
2024-03-24 12:22:07 +0100

    Websocket client - server connection

--- components/queue.tsx
+++ components/queue.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { io } from 'socket.io-client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Queue = () => {
+    const [payloads, setPayloads] = useState<object[]>([]);
+
     useEffect(() => {
         const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_URL}:${process.env.NEXT_PUBLIC_WEBSOCKET_PORT}`);
         socket.on('connect', () => {
@@ -14,11 +16,25 @@ const Queue = () => {
             console.log('client disconnected', socket.id);
         });
 
+        socket.on('queue', (payload) => {
+            if (payload) {
+                setPayloads((prev) => [...prev, payload]);
+                console.log('receiving', JSON.parse(payload));
+            }
+        });
+
         return () => {
             socket.disconnect();
         };
     }, []);
 
-    return <div>Hello from the server</div>;
+    return (
+        <div>
+            {/*{payloads.map((p, i) => (*/}
+            {/*    <div key={i}>{JSON.stringify(p)}</div>*/}
+            {/*))}*/}
+            Queue
+        </div>
+    );
 };
 export default Queue;
--- websocket/server.ts
+++ websocket/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
+import { Kafka, logLevel } from 'kafkajs';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,35 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
+    const upstashKafkaBroker = process.env.NEXT_PUBLIC_UPSTASH_KAFKA_BROKER ?? '';
+    const username = process.env.NEXT_PUBLIC_UPSTASH_KAFKA_REST_USERNAME ?? '';
+    const password = process.env.NEXT_PUBLIC_UPSTASH_KAFKA_REST_PASSWORD ?? '';
+
+    const kafka = new Kafka({
+        brokers: [upstashKafkaBroker],
+        ssl: true,
+        sasl: {
+            mechanism: 'scram-sha-256',
+            username,
+            password
+        },
+        logLevel: logLevel.ERROR
+    });
+
+    const consumer = kafka.consumer({ groupId: socket.id });
+
+    const run = async () => {
+        await consumer.connect();
+        await consumer.subscribe({ topic: 'gitlab-webhook', fromBeginning: true });
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message, heartbeat }) => {
+                socket.emit('queue', message.value?.toString());
+                await heartbeat();
+            }
+        });
+    };
+
+    run().catch((e) => console.error('Upstash Kafka error', e));
     console.log('a user connected');
 });
 
