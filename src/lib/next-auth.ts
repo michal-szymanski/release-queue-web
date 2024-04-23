592c132407978a9f097cd60d2b23cc277275a39d
Michał Szymański <michal.szymanski92@gmail.com>
2024-04-23 17:10:49 +0200

    token rotation

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -2,6 +2,7 @@ import { AuthOptions } from 'next-auth';
 import GitlabProvider from 'next-auth/providers/gitlab';
 import { z } from 'zod';
 import { env } from '@/env';
+import { JWT } from 'next-auth/jwt';
 
 export const authOptions: AuthOptions = {
     pages: {
@@ -21,14 +22,78 @@ export const authOptions: AuthOptions = {
     ],
     callbacks: {
         async jwt({ token, account }) {
-            if (account) {
+            const isTokenActive = Date.now() < (token.expires_at ?? 0) * 1000;
+            const isSignIn = account !== null && account !== undefined;
+
+            if (isTokenActive) {
+                return token;
+            }
+
+            if (isSignIn) {
                 token.access_token = account.access_token;
             }
-            return token;
+
+            return rotatePersonalAccessToken(token);
         },
         async session({ session, token }) {
             session.user.id = z.coerce.number().positive().parse(token.sub);
             return session;
         }
+    }
+};
+
+const personalAccessTokenSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    revoked: z.boolean(),
+    created_at: z.string().datetime(),
+    scopes: z.array(z.string()),
+    user_id: z.number(),
+    last_used_at: z.string().datetime().nullable(),
+    active: z.boolean(),
+    expires_at: z.string(),
+    token: z.string().optional()
+});
+
+type PersonalAccessToken = z.infer<typeof personalAccessTokenSchema>;
+
+const getPersonalAccessToken = async ({ access_token }: JWT): Promise<PersonalAccessToken> => {
+    const response = await fetch(`${env.GITLAB_URL}/api/v4/personal_access_tokens?search=release-queue&revoked=false`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+        return z.array(personalAccessTokenSchema).min(1).parse(json)[0];
+    }
+
+    throw Error('Could not retrieve personal access token.', json);
+};
+
+const rotatePersonalAccessToken = async (token: JWT): Promise<JWT> => {
+    try {
+        const { id } = await getPersonalAccessToken(token);
+        const response = await fetch(`${env.GITLAB_URL}/api/v4/personal_access_tokens/${id}/rotate`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token.access_token}`
+            }
+        });
+
+        const json = await response.json();
+        const { token: newAccessToken, expires_at } = personalAccessTokenSchema.parse(json);
+
+        return {
+            ...token,
+            access_token: newAccessToken,
+            expires_at: new Date(expires_at).getTime() / 1000
+        };
+    } catch (e) {
+        console.error('Could not rotate personal access token.', e);
+        return token;
     }
 };
--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -10,5 +10,6 @@ declare module 'next-auth' {
 declare module 'next-auth/jwt' {
     interface JWT {
         access_token?: string;
+        expires_at?: number;
     }
 }
